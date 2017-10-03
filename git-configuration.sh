08afc7d9cdcb81d5a3581904c59c8b57211f897d
DarkHelix007 <shobhitmaheshwari2003@hotmail.com>
2017-10-03 06:51:38 +0000

    Add git-configuration

---
+++ git-configuration.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This modifies the PS1 to have a more stylish look(according to me)
+# It also enables branch name auto-completion
+
+cd 
+curl -o ~/.git-prompt.sh -OL cdn.learnenough.com/git-prompt.sh
+curl -o ~/.git-completion.bash -OL cdn.learnenough.com/git-completion.bash
+chmod +x ~/.git-prompt.sh
+chmod +x ~/.git-completion.bash
+
+echo '# Git configuration
+# Branch name in prompt
+source ~/.git-prompt.sh
+PS1='[\W$(__git_ps1 " (%s)")]\$ '
+export PROMPT_COMMAND='echo -ne "\033]0;${PWD/#$HOME/~}\007"'
+# Tab completion for branch names
+source ~/.git-completion.bash' >> ~/.bashrc
