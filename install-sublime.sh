2df0c5df8ab5d422a0e33f1e3b64dd1d7269344e
DarkHelix007 <shobhitmaheshwari2003@hotmail.com>
2017-10-03 06:37:41 +0000

    Add sublime-text install

---
+++ install-sublime.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This installs sublime-text stable build
+#Be sure to run sudo apt-get update before this
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get install sublime-text
