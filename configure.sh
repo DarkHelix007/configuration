2f733686f2697f0d27a2723f59c49e43477056ca
DarkHelix007 <shobhitmaheshwari2003@hotmail.com>
2018-03-03 06:29:06 +0000

    Add file to automate everything

---
+++ configure.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Just do everything
+source install-sublime.sh
+source git-configuration.sh
+source ruby-setup.sh
--- ruby-setup.sh
+++ ruby-setup.sh
@@ -6,11 +6,11 @@ echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 mkdir ~/.rbenv
 mkdir ~/.rbenv/plugins
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-rbenv install 2.3.3
-rbenv rehash
+#rbenv install 2.3.3
+#rbenv rehash
 #Set ruby v2.3.3 as default
-rbenv global 2.3.3
+#rbenv global 2.3.3
 
 #This part excludes the download of ruby index and ruby docs while installing gems
-mkdir ~/.gemrc
+subl ~/.gemrc
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
